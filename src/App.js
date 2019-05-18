6830fef78b0c1ca067dacd8d373173c3c25bfbe6
abdisalanvp77 <abdisalanvp77@gmail.com>
2019-05-18 01:31:52 -0400

    updated state and events

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Meteorite from './components/Meteorite';
 import SearchPanel from './components/SearchPanel';
 class App extends Component{
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       meteors: [
         {id: 1,
--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
 
 class SearchPanel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            meteorDetail: "",
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleClick(e) {
+        e.preventDefault();
+        console.log('The link was clicked.');
+    }
+    handleChange(e){
+        e.preventDefault();
+        this.setState(state => ({
+            meteorDetail: e.target.value,
+        }));
+
+    }
+
+ 
     render() {
         return (
             <div>
-                <input type="text" placeholder="Search meteorite..."/>
-                <button type="submit">Search</button>
+                
+                <input type="text" placeholder="Search meteorite..." onChange={this.handleChange}/>
+                <button type="submit" onClick={this.handleClick}>Search</button>
+                <p>SEARCH: {this.state.meteorDetail}</p>
             </div>
         );
 
